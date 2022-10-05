3dfa2657c66805db5000ef4dd1b8c104948b54e5
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-10-05 22:54:04 +0200

    Update removeholesopt.cpp

--- wlg_original_test/functionspace/removeholesopt.cpp
+++ wlg_original_test/functionspace/removeholesopt.cpp
@@ -2,28 +2,14 @@
 using namespace cv;
 #define ZERO 0
  void WatershedAlg::removeholesopt(Mat& src,int pnumThrshold, int handlingType, int neighbourType,int pixelThreshold)  {
-   
-   
- //  cv::imshow("distanceTransform before remove holes",src);
- //  cv::waitKey(0);
+
     int RemoveCount=0;       
-    //string filename33="/opt/test33.txt";
-   // ofstream fout33(filename33);
- //   string filename34="/opt/test34.txt";
- //   ofstream fout34(filename34);
-   //  string filename35="/opt/test35.txt";
-  //  ofstream fout35(filename35);
-   // vector<Point> NeihborPos;
-    //queue<int> qx, qy;
-    //vector<Point>pixbuffer;
+
     Mat Pointlabel(src.rows,src.cols, CV_8UC1,Scalar::all(255) );  
     
- 
- 
-       // cout<<"removing holes start. "<<endl;  
+
         for(int i = 0; i < src.rows; ++i)  {    
-           // uchar* iData = src.ptr<uchar>(i);  
-           // uchar* iLabel = Pointlabel.ptr<uchar>(i);  
+ 
            
             for(int j = 0; j < src.cols; ++j) {    
                  if(src.at<uchar>(i,j)>pixelThreshold){
@@ -31,21 +17,11 @@ using namespace cv;
                  
                
                  }
-               // if (iData[j] > pixelThreshold)    
-              //  {    
-                   // iLabel[j] = 3;   
-              //  }    
+    
             }    
         }  
 
-//**
 
- //  imshow("prepration for contours",Pointlabel);
-//
-  //  cv::waitKey(0);
-
-   // Mat dulpPointlabel;
-    //Pointlabel.copyTo(dulpPointlabel);
     vector<vector<Point>> contours;  
     vector<cv::Vec4i> hierarchy; 
     findContours(Pointlabel,contours,hierarchy,RETR_TREE,CHAIN_APPROX_SIMPLE,Point()); 
@@ -58,7 +34,7 @@ using namespace cv;
    if (!contours.empty() && !hierarchy.empty()) 
 	{
 		vector<vector<Point> >::const_iterator itc = contours.begin();
-   // cout<<"The number of size of contours"<<contours.size()<<endl;
+
 	
 		while (itc != contours.end()) 
 		{
@@ -89,68 +65,5 @@ using namespace cv;
 		}
 	}
 
-/*
-       for(int i=0;i<Pointlabel.rows;i++){
-           for(int j=0;j<Pointlabel.cols;j++){
-              fout34<<(int)Pointlabel.at<uchar>(i,j)<<",";
-             
-
-         }
-           fout34<<endl;
-      }
-           fout34.close();
-
-*/
-
-
-/*
-
-
-    for(int i=0;i<contours.size();i++)  
-    {  
-        for(int j=0;j<contours[i].size();j++)   
-        {  
-           p=Point(contours[i][j].x,contours[i][j].y);  
-            contouroutput.at<uchar>(p)=255;  
-        }  
-  
-        //输出hierarchy向量内容  
-       // char ch[256];  
-       // sprintf(ch,"%d",i);  
-       // string str=ch;  
-       // cout<<"向量hierarchy的第" <<str<<" 个元素内容为："<<endl<<hierarchy[i]<<endl<<endl;  
-  
-        //绘制轮廓  
-        drawContours(imageContours,contours,i,Scalar(255),1,8,hierarchy);  
-    }  
-    
-     
-    imshow("Point of Contours :contouroutput",contouroutput);   //向量contours内保存的所有轮廓点集  
-    waitKey(0);  
-    imshow("Contours Image:imageContours",imageContours); //轮廓  
-    waitKey(0); 
-
-
-
-
-//**
-
-
-*/
-
-     /*  for(int i=0;i<dulpPointlabel.rows;i++){
-           for(int j=0;j<dulpPointlabel.cols;j++){
-              fout33<<(int)dulpPointlabel.at<uchar>(i,j)<<",";
-             
-
-         }
-           fout33<<endl;
-      }
-           fout33.close();
-
-
-
-
-       */
 
  }
